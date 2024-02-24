64b20ae57e1f1ea36ce64afb8b50f86e75444c50
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-25 00:09:35 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <>
+      <div className=" w-full h-screen bg-pink-800 overflow-auto">
+        <div className="flex">
+          <div>foto</div>
+          <div>form</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
